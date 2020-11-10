bc9f6afa4c492deef2ef56373d088ec6fc9b72d3
TomekLesniak <tomasz.lesniak152@gmail.com>
2020-11-10 17:55:30 +0100

    Extract method to get category enum

--- lib/providers/dishes.dart
+++ lib/providers/dishes.dart
@@ -8,81 +8,97 @@ class Dishes with ChangeNotifier {
   List<Dish> _dishes = [];
 
   List<Dish> getDishes(String categoryName) {
+    MealCategory selectedCategory = _getCategoryFromName(categoryName);
+    return _dishes.where((dish) => dish.category == selectedCategory).toList();
+  }
+
+  MealCategory _getCategoryFromName(String categoryName) {
     categoryName = categoryName.toLowerCase();
-    MealCategory selectedCategory;
-    switch(categoryName) {
+    switch (categoryName) {
       case 'breakfest':
-        selectedCategory = MealCategory.breakfast;
+        return MealCategory.breakfast;
         break;
       case 'secondbreakfest':
-        selectedCategory = MealCategory.secondBreakfest;
+        return MealCategory.secondBreakfest;
         break;
       case 'dinner':
-        selectedCategory = MealCategory.dinner;
+        return MealCategory.dinner;
         break;
       case 'supper':
-        selectedCategory = MealCategory.supper;
+        return MealCategory.supper;
         break;
+      default:
+        throw Exception('Category not implemented');
     }
-    return _dishes.where((dish) => dish.category == selectedCategory).toList();
   }
 
   Future<void> fetchAndSetDishes() async {
     var url = 'https://easydiet-9b265.firebaseio.com/dishes.json';
 
-    if(_dishes.isEmpty) {
+    if (_dishes.isEmpty) {
       final response = await http.post(url,
           body: json.encode({
             'name': 'breakfest',
             'ingredients': ['Bread', 'test']
           }));
 
-    
-    print(response.body);
-    
-    _dishes = [
-      Dish(
-          id: 'd1',
-          calories: 400,
-          carbohydrates: 20,
-          fat: 10,
-          proteins: 30,
-          name: 'breakfest',
-          ingredients: ['Bread', 'test', 'another_one'],
-          category: MealCategory.breakfast,
-          imageUrl: 'https://miro.medium.com/max/10000/0*wZAcNrIWFFjuJA78'),
-          Dish(
-          id: 'd3',
-          calories: 400,
-          carbohydrates: 20,
-          fat: 10,
-          proteins: 30,
-          name: 'breakfest2',
-          ingredients: ['Bread', 'test', 'another_one'],
-          category: MealCategory.breakfast,
-          imageUrl: 'https://miro.medium.com/max/10000/0*wZAcNrIWFFjuJA78'),
-          Dish(
-          id: 'd2',
-          calories: 400,
-          carbohydrates: 20,
-          fat: 10,
-          proteins: 30,
-          name: 'breakfest',
-          ingredients: ['Bread', 'test', 'another_one'],
-          category: MealCategory.breakfast,
-          imageUrl: 'https://miro.medium.com/max/10000/0*wZAcNrIWFFjuJA78'),
-      Dish(
-          id: 'd4',
-          calories: 400,
-          carbohydrates: 20,
-          fat: 10,
-          proteins: 30,
-          name: 'Dinner',
-          ingredients: ['Rice', 'rice', 'another_one','Rice', 'rice', 'another_one','Rice', 'rice', 'another_one'],
-          category: MealCategory.dinner,
-          imageUrl: 'https://miro.medium.com/max/10000/0*wZAcNrIWFFjuJA78'),
-    ];
-    notifyListeners();
+      print(response.body);
+
+      _dishes = [
+        Dish(
+            id: 'd1',
+            calories: 400,
+            carbohydrates: 20,
+            fat: 10,
+            proteins: 30,
+            name: 'breakfest',
+            ingredients: ['Bread', 'test', 'another_one'],
+            category: MealCategory.breakfast,
+            imageUrl: 'https://miro.medium.com/max/10000/0*wZAcNrIWFFjuJA78'),
+        Dish(
+            id: 'd3',
+            calories: 400,
+            carbohydrates: 20,
+            fat: 10,
+            proteins: 30,
+            name: 'breakfest2',
+            ingredients: ['Bread', 'test', 'another_one'],
+            category: MealCategory.breakfast,
+            imageUrl: 'https://miro.medium.com/max/10000/0*wZAcNrIWFFjuJA78'),
+        Dish(
+            id: 'd2',
+            calories: 400,
+            carbohydrates: 20,
+            fat: 10,
+            proteins: 30,
+            name: 'breakfest',
+            ingredients: ['Bread', 'test', 'another_one'],
+            category: MealCategory.breakfast,
+            imageUrl: 'https://miro.medium.com/max/10000/0*wZAcNrIWFFjuJA78'),
+        Dish(
+            id: 'd4',
+            calories: 400,
+            carbohydrates: 20,
+            fat: 10,
+            proteins: 30,
+            name: 'Dinner',
+            ingredients: [
+              'Rice',
+              'rice',
+              'another_one',
+              'Rice',
+              'rice',
+              'another_one',
+              'Rice',
+              'rice',
+              'another_one'
+            ],
+            category: MealCategory.dinner,
+            imageUrl: 'https://miro.medium.com/max/10000/0*wZAcNrIWFFjuJA78'),
+      ];
+      notifyListeners();
     }
   }
+
+  
 }
