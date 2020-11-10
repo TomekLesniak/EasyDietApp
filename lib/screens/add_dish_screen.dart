dceadeea5ab851f997e208d47758745ea70eb740
TomekLesniak <tomasz.lesniak152@gmail.com>
2020-11-10 22:29:18 +0100

    New dish form all input fields

--- lib/screens/add_dish_screen.dart
+++ lib/screens/add_dish_screen.dart
@@ -27,62 +27,139 @@ class _AddDishScreenState extends State<AddDishScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Add new dish!'),
-        ),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Card(
-              elevation: 5,
-              margin: EdgeInsets.all(12),
-              child: Padding(
-                padding: EdgeInsets.all(12),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        decoration: InputDecoration(
-                            labelText: 'Name',
-                            labelStyle: Theme.of(context).textTheme.headline6),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Divider(thickness: 2,),
-                      formText(context, 'Category:'),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          formText(context, 'Breakfest'),
-                          FormRadio(MealCategory.breakfast, _radioValue,
-                              _handleRadioValueChange),
-                          formText(context, 'Second Breakfest'),
-                          FormRadio(MealCategory.secondBreakfest, _radioValue,
-                              _handleRadioValueChange),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          formText(context, 'Dinner'),
-                          FormRadio(MealCategory.dinner, _radioValue,
-                              _handleRadioValueChange),
-                          formText(context, 'Supper'),
-                          FormRadio(MealCategory.supper, _radioValue,
-                              _handleRadioValueChange),
-                        ],
-                      ),
-                      Divider(thickness: 2,),
-                    ],
-                  ),
+      appBar: AppBar(
+        title: Text('Add new dish!'),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Card(
+            elevation: 5,
+            margin: EdgeInsets.all(12),
+            child: Padding(
+              padding: EdgeInsets.all(12),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                          labelText: 'Name',
+                          labelStyle: Theme.of(context).textTheme.headline6),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Divider(
+                      thickness: 2,
+                    ),
+                    formText(context, 'Category:'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        formText(context, 'Breakfest'),
+                        FormRadio(MealCategory.breakfast, _radioValue,
+                            _handleRadioValueChange),
+                        formText(context, 'Second Breakfest'),
+                        FormRadio(MealCategory.secondBreakfest, _radioValue,
+                            _handleRadioValueChange),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        formText(context, 'Dinner'),
+                        FormRadio(MealCategory.dinner, _radioValue,
+                            _handleRadioValueChange),
+                        formText(context, 'Supper'),
+                        FormRadio(MealCategory.supper, _radioValue,
+                            _handleRadioValueChange),
+                      ],
+                    ),
+                    Divider(
+                      thickness: 2,
+                    ),
+                    Row(
+                      children: [
+                        Flexible(
+                          child: TextFormField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                                labelText: 'Calories',
+                                labelStyle:
+                                    Theme.of(context).textTheme.headline6),
+                          ),
+                        ),
+                        SizedBox(width: 30),
+                        Flexible(
+                          child: TextFormField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                                labelText: 'Carbohydrates',
+                                labelStyle:
+                                    Theme.of(context).textTheme.headline6),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Flexible(
+                          child: TextFormField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                                labelText: 'Fat',
+                                labelStyle:
+                                    Theme.of(context).textTheme.headline6),
+                          ),
+                        ),
+                        SizedBox(width: 30),
+                        Flexible(
+                          child: TextFormField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                                labelText: 'Proteins',
+                                labelStyle:
+                                    Theme.of(context).textTheme.headline6),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black),
+                          ),
+                          width: 125,
+                          height: 100,
+                          child: Image.network(
+                            'https://miro.medium.com/max/10000/0*wZAcNrIWFFjuJA78',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Flexible(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                  labelText: 'Url',
+                                  labelStyle:
+                                      Theme.of(context).textTheme.headline6),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
